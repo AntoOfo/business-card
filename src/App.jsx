8abf2b41a3158e0892e2fbda639bd40a20bdf157
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-08-28 23:13:04 +0100

    footer added with two images. added to app file and styled. two separate divs in app, one for main content w padding and the footer w/o

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Info from "./components/Info"
 import About from "./components/About"
 import Interests from "./components/Interests"
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
     <>
     <div className="info-card">
+      <div style={{padding: "20px"}}>  {/* separate padding from footer */}
       <Info />
       <About />
       <Interests />
+      </div>
+      <Footer />
     </div>
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <img src="src/assets/github-footer.png" alt="github image" />
+            <img src="src/assets/linkedin-footer.png" alt="github image" />
+        </footer>
+    )
+}
