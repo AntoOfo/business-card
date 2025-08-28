31cfd00b1b031f1c0e4beea74417e796e4639256
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-08-28 21:32:36 +0100

    comments added to styling for future reference. also other past changes added

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,8 +4,14 @@ export default function Info() {
             <img src="src/assets/placeholder.jpg" alt="placeholder image"/>
             <h1>Antonio Ofodile</h1>
             <p>Junior Front End Developer</p>
-            <button type="button">Email</button>
-            <button type="button">LinkedIn</button>
+            <div className="buttons-div">
+            <button className="email-btn"><img src="src/assets/email.png" alt="email icon" className="btn-icon" />
+                Email
+            </button>
+            <button className="link-btn"><img src="src/assets/linkedin.png" alt="email icon" className="btn-icon" />
+                LinkedIn
+            </button>
+            </div>
         </header>
     )
 }
