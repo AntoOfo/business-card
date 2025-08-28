6fef751967840a7490026b611610efdb71b9bd04
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-08-28 23:36:22 +0100

    about section shortened and margin values changed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ export default function About() {
         <>
             <div className="about-div">
             <h2>About</h2>
-            <p>Hey! I'm a frontend developer who loves turning ideas into smooth, easy-to-use apps. I mess with new tools, experiment with fun ways to improve my projects, and keep my code clean... mostly. Always curious, always building, and probably thinking about my next cool project.</p>
+            <p>Hey! I'm a frontend developer who loves turning ideas into smooth apps. I like to mess with new tools, keep my code clean… mostly, and I'm always thinking about my next project.</p>
             </div>
         </>
     )
