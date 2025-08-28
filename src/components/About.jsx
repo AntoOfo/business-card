e6ca4af8167415a25ea830625fdce7cb62c062b8
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-08-28 22:01:43 +0100

    about file created with html and imported in app file. color and font changes added to text

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return(
+        <>
+            <div className="about-div">
+            <h2>About</h2>
+            <p>Hey! I'm a frontend developer who loves turning ideas into smooth, easy-to-use apps. I mess with new tools, experiment with fun ways to improve my projects, and keep my code clean... mostly. Always curious, always building, and probably thinking about my next cool project.</p>
+            </div>
+        </>
+    )
+}
