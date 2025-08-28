bc36ce2a365aa120619a65173e4d2c3454dce7f6
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-08-28 18:38:59 +0100

    placeholder image added, initial tags created, colours added. working on styling the info section 1st

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
+import Info from "./components/Info"
 
+export default function App() {
+  return (
+    <>
+    <div className="info-card">
+      <Info />
+    </div>
+    </>
+  )
+}
---
+++ src/components/Info.jsx
@@ -0,0 +1,11 @@
+export default function Info() {
+    return (
+        <header>
+            <img src="src/assets/placeholder.jpg" alt="placeholder image"/>
+            <h1>Antonio Ofodile</h1>
+            <p>Junior Front End Developer</p>
+            <button type="button">Email</button>
+            <button type="button">LinkedIn</button>
+        </header>
+    )
+}
