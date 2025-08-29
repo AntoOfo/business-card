189ca26f903af3eaf7758df41ef99be4ed66b46f
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-08-29 14:35:22 +0100

    info button links added

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,13 +4,21 @@ export default function Info() {
             <img src="src/assets/placeholder.jpg" alt="placeholder image"/>
             <h1>Antonio Ofodile</h1>
             <p>Junior Front End Developer</p>
+            
             <div className="buttons-div">
+
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=antonioofodileuk@gmail.com">
             <button className="email-btn"><img src="src/assets/email.png" alt="email icon" className="btn-icon" />
                 Email
             </button>
+            </a>
+
+            <a href="https://www.linkedin.com/in/antonioofodile/">
             <button className="link-btn"><img src="src/assets/linkedin.png" alt="email icon" className="btn-icon" />
                 LinkedIn
             </button>
+            </a>
+
             </div>
         </header>
     )
