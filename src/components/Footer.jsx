c892d253188c044445197b56518138cce9490349
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-08-29 15:28:07 +0100

    links added to footer images and footer imgs styling route changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,13 @@
 export default function Footer() {
     return (
         <footer className="footer">
+            <a href="https://github.com/AntoOfo">
             <img src="src/assets/github-footer.png" alt="github image" />
+            </a>
+
+            <a href="https://www.linkedin.com/in/antonioofodile/">
             <img src="src/assets/linkedin-footer.png" alt="github image" />
+            </a>
         </footer>
     )
 }
